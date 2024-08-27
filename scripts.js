c330c99ea5368fba1222a1499554ce8c1d4ff239
Kirill <krowllirik@gmail.com>
2024-08-27 17:01:17 +0400

    changes connection to database

--- scripts.js
+++ scripts.js
@@ -1,9 +1,14 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://bqwbkywxuzkdoohwfvoo.supabase.co';
+const supabaseKey = process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJxd2JreXd4dXprZG9vaHdmdm9vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQ3NjE3NDgsImV4cCI6MjA0MDMzNzc0OH0.zGFihxVjN8NpD0OfIR3Lr - hdNo2A3yPV9dl0Xnk7KfM;
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 // Инициализация Supabase
-const supabaseUrl = 'https://bqwbkywxuzkdoohwfvoo.supabase.com';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJxd2JreXd4dXprZG9vaHdmdm9vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQ3NjE3NDgsImV4cCI6MjA0MDMzNzc0OH0.zGFihxVjN8NpD0OfIR3Lr-hdNo2A3yPV9dl0Xnk7KfM';
-const supabase = createClient(supabaseUrl, supabaseKey);
+//const supabaseUrl = 'https://bqwbkywxuzkdoohwfvoo.supabase.com';
+//const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJxd2JreXd4dXprZG9vaHdmdm9vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQ3NjE3NDgsImV4cCI6MjA0MDMzNzc0OH0.zGFihxVjN8NpD0OfIR3Lr-hdNo2A3yPV9dl0Xnk7KfM';
+//const supabase = createClient(supabaseUrl, supabaseKey);
 
 // Обработка формы
 const form = document.getElementById('astrology-form');
