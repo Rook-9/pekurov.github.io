e4ee08524dcf636ffdc0c031a94b3b724f391796
Kirill <krowllirik@gmail.com>
2024-09-02 13:37:43 +0400

    add crossbrowser tests

--- test_sample.py
+++ test_sample.py
@@ -1,9 +1,18 @@
 from playwright.sync_api import sync_playwright
 
-def test_status_code_200():
+def run_test_in_browser(browser_type):
     with sync_playwright() as p:
-        browser = p.chromium.launch()
+        browser = p[browser_type].launch()
         page = browser.new_page()
-        response = page.goto('https://rook-9.github.io/pekurov.github.io/')  # Замените на URL вашего сайта
+        response = page.goto('https://rook-9.github.io/pekurov.github.io/')
         assert response.status == 200
         browser.close()
+
+def test_status_code_200_chromium():
+    run_test_in_browser("chromium")
+
+def test_status_code_200_firefox():
+    run_test_in_browser("firefox")
+
+def test_status_code_200_webkit():
+    run_test_in_browser("webkit")
